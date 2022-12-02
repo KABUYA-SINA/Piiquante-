8ce8c8919b296347821aa300c2940375e7d64766
KABUYA <kabuya-sina@netcourrier.com>
2022-12-02 12:46:12 +0100

    updates

--- back-end/controllers/controller.js
+++ back-end/controllers/controller.js
@@ -2,10 +2,12 @@ const Sauce = require('../models/saucemodel')
 const fs = require('fs')
 
 
-const likes = 0
-const dislikes = 0
-const usersLiked = []
-const usersDisliked = []
+
+exports.GetAllObjet = (req, res, next) => {
+    Sauce.find()
+        .then(sauces => res.status(200).json(sauces)) //-------------------------------------------------{}
+        .catch(error => res.status(400).json({ error }));
+}
 exports.CreateNewObjects = (req, res, next) => {
     const SauceObject = JSON.parse(req.body.sauce)
     delete SauceObject._id
@@ -13,11 +15,21 @@ exports.CreateNewObjects = (req, res, next) => {
     const sauce = new Sauce({
         ...SauceObject,
         userId: req.auth.userId,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        likes: 0,
+        dislikes: 0,
+        usersLiked: [],
+        usersDisliked: []
     })
     sauce.save()
         .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
         .catch(error => res.status(400).json({ error }));
+}
+
+exports.GetOneObjet = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json( sauce ))  //-------------------------------------------------{}
+        .catch(error => res.status(404).json({ error }));
 }
 
 
@@ -63,14 +75,4 @@ exports.DeleteObjet = (req, res, next) => {
         });
 };
 
-exports.GetOneObjet = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-        .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({ error }));
-}
 
-exports.GetAllObjet = (req, res, next) => {
-    Sauce.find()
-        .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({ error }));
-}
